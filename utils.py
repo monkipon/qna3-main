cdb5e74fff9941a3573671e216a2899ac4220041
monkipon <154270946+monkipon@users.noreply.github.com>
2024-01-03 23:11:11 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import aiofiles
-
+import art
 
 async def read_private_keys(file_path):
     async with aiofiles.open(file_path, 'r') as file:
@@ -17,3 +17,9 @@ async def read_proxies(file_path):
             except IndexError:
                 print(f"Ignore invalid line: {line}")
     return proxies
+
+def make_art():
+    art_text = art.text2art('Qna3')
+    lines = "-" * len(art_text.split('\n')[0])
+    print(f"{lines}\n{art_text}{lines}")
+    print('Создатель: https://t.me/Genjurx')
