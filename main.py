62e34f4890162a240bd9cf90589fd6201e8998c1
monkipon <154270946+monkipon@users.noreply.github.com>
2024-01-11 22:21:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,29 @@ import time
 import art
 from utils import *
 import asyncio
-from config import THREADS
+from config import *
 
 
 async def process_private_key(private_key, proxy, semaphore):
     async with semaphore:
         qna3_bot = Qna3(private_key, proxy)
-        await qna3_bot.get_graphl()
-
-        if qna3_bot.todayCount == 0:
-            result = await qna3_bot.verify_transaction()
-            print(f"{qna3_bot.account.address} | {result} | {qna3_bot.proxy_ip}")
-        else:
-            print(f"{qna3_bot.account.address} | {qna3_bot.checkInDays} | {qna3_bot.todayCount} | {qna3_bot.proxy_ip}")
+        await qna3_bot.get_token()
+        if qna3_bot.token:
+            if CLAIM:
+                hash = await qna3_bot.claim_all_tx()
+                if hash:
+                    print(f"{qna3_bot.account.address} | {hash} | {qna3_bot.proxy_ip}")
+                else:
+                    print(f"{qna3_bot.account.address} | 'Already Claimed' | {qna3_bot.proxy_ip}")
+            else:
+                await qna3_bot.get_graphl()
+                if qna3_bot.todayCount == 0:
+                    result = await qna3_bot.verify_transaction()
+                    print(f"{qna3_bot.account.address} | {result} | {qna3_bot.proxy_ip}")
+                else:
+                    print(f"{qna3_bot.account.address} | Streak: {qna3_bot.checkInDays} | {qna3_bot.proxy_ip}")
 
         await qna3_bot.close_session()
-
-
 
 
 def make_art():
@@ -35,16 +41,18 @@ async def main():
     time1 = time.time()
     private_keys = await read_private_keys('./inputs/keys.txt')
     proxies = await read_proxies('./inputs/proxies.txt')
-
+    # print(proxies)
     semaphore = asyncio.Semaphore(THREADS)
 
-    tasks = [process_private_key(private_key, proxies[i % len(proxies)], semaphore) for i, private_key in enumerate(private_keys)]
+    tasks = [process_private_key(private_key, proxies[i % len(proxies)], semaphore) for i, private_key in
+             enumerate(private_keys)]
 
     await asyncio.gather(*tasks)
 
     time2 = time.time()
     final_time = time2 - time1
     print(final_time)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
